9a1dd6501944c3c0c6418a09c5c49f63ab769d39
davi-ldf <davifranca135@gmail.com>
2025-01-10 17:21:02 -0300

    listing users

--- public/main.js
+++ public/main.js
@@ -17,7 +17,20 @@ function renderUserList () {
 
     userList.forEach(i => {
         ul.innerHTML += `<li>${i}</li>`;
-    })
+    });
+}
+
+function addMessage(type, user, msg) {
+    let ul = document.querySelector('.chatList');
+
+    switch(type) {
+        case 'status':
+            ul.innerHTML += '<li class="m-status">'+msg+"</li>";
+        break;
+        case 'msg':
+            ul.innerHTML += '<li class="m-txt"><span>'+user+'</span>'+msg+"</li>";
+        break;
+    }
 }
 
 
@@ -38,7 +51,22 @@ socket.on('user-ok', (list) => {
     chatPage.style.display = 'flex';
     textInput.focus();
 
+    addMessage('status', null, 'Conectado!');
+
     userList = list;
     renderUserList();
 
+});
+
+socket.on('list-update', (data) => {
+    if(data.joined) {
+        addMessage('status', null, data.joined+' entrou no chat');
+    }
+
+    if(data.left) {
+        addMessage('status', null, data.left+' saiu do chat');
+    }
+
+    userList = data.list;
+    renderUserList();
 });
